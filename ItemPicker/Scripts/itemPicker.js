1583357859a1f5c820ad2124e6588aa8d8939458
Daniel Kolman <daniel@kolman.cz>
2012-02-15 15:07:46 +0100

    Showing images

--- ItemPicker/Scripts/itemPicker.js
+++ ItemPicker/Scripts/itemPicker.js
@@ -1,5 +1,6 @@
 ﻿(function ($) {
   var defaults = {
+    multiselect: false,
     getFieldName: function ($picker) { },
     loadContent: function ($picker, callback) { }
   };
@@ -56,14 +57,20 @@
     var fieldName = settings.getFieldName($picker);
     if (!fieldName) throw 'Field name must be set';
     for (var i = 0; i < content.length; i++) {
-      createItem($container, fieldName, content[i]);
+      var $itemContainer = $('<div class="itemPicker-item"></div>').appendTo($container);
+      createItem($itemContainer, fieldName, settings.multiselect, content[i]);
     }
   }
 
-  function createItem($container, fieldName, item) {
-    var $checkbox = $('<input type="checkbox" />').attr('value', item.Value).attr('name', fieldName);
-    var $label = $('<label></label>').text(item.Label).prepend($checkbox);
-    var $item = $('<div class="itemPicker-item"></div>').append($label);
-    $container.append($item);
+  function createItem($container, fieldName, multiselect, item) {
+    var $label = $('<label></label>').appendTo($container);
+
+    $('<input type="checkbox"/>').attr('value', item.Value).attr('name', fieldName).appendTo($label);
+
+    if (item.Thumbnail) {
+      $('<img/>').attr('src', item.Thumbnail).appendTo($label);
+    }
+    
+    $('<span></span>').text(item.Label).appendTo($label);
   }
 })(jQuery);
