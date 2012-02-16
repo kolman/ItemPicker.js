0a0a4f77b8d8de58cf64da3331025530de93fd5f
Daniel Kolman <daniel@kolman.cz>
2012-02-16 13:26:50 +0100

    Automation of QUnit tests using SQUnit

---
+++ ItemPicker/test/RunTests.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using SQUnit;
+
+namespace ItemPicker.test
+{
+	public class RunTests
+	{
+		TestRunner _runner;
+
+		[TestFixtureSetUp]
+		public void TestFixtureSetUp()
+		{
+			_runner = new TestRunner();
+		}
+
+		[TestFixtureTearDown]
+		public void TestFixtureTearDown()
+		{
+			_runner.Dispose();
+		}
+
+		static readonly string TestDirectory = Path.GetFullPath(@"..\test");
+		public static string[] GetAllQUnitTestFiles()
+		{
+			return Directory
+				.GetFiles(TestDirectory, "*.htm*")
+				.Select(Path.GetFileName)
+				.ToArray();
+		}
+
+		[Test]
+		[TestCaseSource("GetAllQUnitTestFiles")]
+		public void RunQUnitTests(string file)
+		{
+			var fullPath = Path.Combine(TestDirectory, file);
+			var results = _runner.RunTestsInFile(fullPath);
+			results.AssertAllTestsPassed();
+		}
+	}
+}
